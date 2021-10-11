cd8ae018541da68c17b8ff05af36edb22b766ae5
Eman Khalifa <khalifaeman4@gmail.com>
2021-10-11 16:17:40 +0200

    start commit for cookie cutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Start of test DS project',
+    author='Eman Khalifa',
+    license='MIT',
+)
